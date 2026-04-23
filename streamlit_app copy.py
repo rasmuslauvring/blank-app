aebe4b310e149118d5c3ed5462e4d7a594aef331
rasmuslauvring <Rasmus.Lauvring@mpmd-htwberlin.de>
2026-04-23 09:56:51 +0200

    asdf

---
+++ streamlit_app copy.py
@@ -0,0 +1,33 @@
+import streamlit as st
+
+st.title("🎈 My new app Rasmus L")
+st.write(
+    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+)
+import streamlit as st
+
+col1, col2, col3 = st.columns(3)
+
+tab1, tab2, tab3 = st.tabs(["Cat", "Dog", "Owl"])
+
+with tab1:
+    st.header("A cat")
+    st.image("https://static.streamlit.io/examples/cat.jpg", width=200)
+with tab2:
+    st.header("A dog")
+    st.image("https://static.streamlit.io/examples/dog.jpg", width=200)
+with tab3:
+    st.header("An owl")
+    st.image("https://static.streamlit.io/examples/owl.jpg", width=200)
+
+with col1:
+    st.header("A cat")
+    st.image("https://static.streamlit.io/examples/cat.jpg")
+
+with col2:
+    st.header("A dog")
+    st.image("https://static.streamlit.io/examples/dog.jpg")
+
+with col3:
+    st.header("An owl")
+    st.image("https://static.streamlit.io/examples/owl.jpg")
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,33 +1,48 @@
+﻿from pathlib import Path
+
 import streamlit as st
 
-st.title("🎈 My new app Rasmus L")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
-import streamlit as st
 
-col1, col2, col3 = st.columns(3)
+IMAGE_DIR = Path(__file__).parent / "images"
 
-tab1, tab2, tab3 = st.tabs(["Cat", "Dog", "Owl"])
+st.title("Introduction to Data Mining")
 
-with tab1:
-    st.header("A cat")
-    st.image("https://static.streamlit.io/examples/cat.jpg", width=200)
-with tab2:
-    st.header("A dog")
-    st.image("https://static.streamlit.io/examples/dog.jpg", width=200)
-with tab3:
-    st.header("An owl")
-    st.image("https://static.streamlit.io/examples/owl.jpg", width=200)
+left_column, right_column = st.columns([3, 2], gap="large", vertical_alignment="top")
 
-with col1:
-    st.header("A cat")
-    st.image("https://static.streamlit.io/examples/cat.jpg")
+with right_column:
+    st.space("large")
+    st.image(
+        str(IMAGE_DIR / "crisp-dm.svg"),
+        width="stretch",
+        caption="CRISP-DM: Cross-Industry Standard Process for Data Mining"
+    )
 
-with col2:
-    st.header("A dog")
-    st.image("https://static.streamlit.io/examples/dog.jpg")
+with left_column:
+    tab_ph01, tab_ph02, tab_ph03, tab_ph04, tab_ph05, tab_ph06 = st.tabs([
+            "01: Business Understanding", "02: Data Understanding", "03: Data Preparation", "04: Modeling", "05: Evaluation", "06: Deployment"
+        ], 
+    )
+    
+    with tab_ph01:
+        st.subheader("Phase 1: Business Understanding")
+        st.image(str(IMAGE_DIR / "crisp-dm-phase01.svg"), width="stretch", caption="Phase 1: Business Understanding")
 
-with col3:
-    st.header("An owl")
-    st.image("https://static.streamlit.io/examples/owl.jpg")
+    with tab_ph02:
+        st.subheader("Phase 2: Data Understanding")
+        st.image(str(IMAGE_DIR / "crisp-dm-phase02.svg"), width="stretch", caption="Phase 2: Data Understanding")
+    
+    with tab_ph03:
+        st.subheader("Phase 3: Data Preparation")
+        st.image(str(IMAGE_DIR / "crisp-dm-phase03.svg"), width="stretch", caption="Phase 3: Data Preparation")
+    
+    with tab_ph04:
+        st.subheader("Phase 4: Modeling")
+        st.image(str(IMAGE_DIR / "crisp-dm-phase04.svg"), width="stretch", caption="Phase 4: Modeling")
+
+    with tab_ph05:
+        st.subheader("Phase 5: Evaluation")
+        st.image(str(IMAGE_DIR / "crisp-dm-phase05.svg"), width="stretch", caption="Phase 5: Evaluation")
+    
+    with tab_ph06:
+        st.subheader("Phase 6: Deployment")
+        st.image(str(IMAGE_DIR / "crisp-dm-phase06.svg"), width="stretch", caption="Phase 6: Deployment")
